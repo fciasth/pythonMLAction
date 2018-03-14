1b7920ff6fcb5d3f1ff55cf6c0525d4f0a9f96d6
zxh <zxh_sea@outlook.com>
2018-03-14 10:41:26 +0800

    按照给定特征划分数据集

--- 03/tree.py
+++ 03/tree.py
@@ -25,8 +25,26 @@ def createDataSet():
                [0,1,'no']]
     labels = ['no Surfacing','flippers']
     return dataSet,labels
-# myDat,labels = createDataSet()
+myDat,labels = createDataSet()
 # a= calcShannonEnt(myDat)
 # print(a)
 
 #划分数据集
+#三个输入参数：待划分的数据集、划分数据集的特征、需要返回的特征的值
+def splitDataSet(dataSet,axis,value):
+    # Python语言不用考虑内存分配问题。 Python语言在函数中传递的是列表的引用，
+    # 在函数内部对列表对象的修改，将会影响该列表对象的整个生存周期。为了消除这
+    # 个不良影响，我们需要在函数的开始声明一个新列表对象。
+    retDataSet = []
+    for featVec in dataSet:
+       # print(featVec[axis])
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            # a[2:4]  从第2个元素开始，到第4个为止的元素。包括头不包括尾。
+            reducedFeatVec.extend(featVec[axis+1:])
+
+            #上面两行就是把作为特征的一列去掉了
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+a= splitDataSet(myDat,2,"no")
+#print(a)
