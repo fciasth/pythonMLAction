f59bfad49c98bc40274c53c2de4c2a9cbc9f9d45
zxh <zxh_sea@outlook.com>
2018-03-19 13:35:12 +0800

    准备数据：从文本中构建词向量

---
+++ 04/bayes.py
@@ -0,0 +1,19 @@
+from numpy import *
+
+def loadDataSet():
+    postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
+                 ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
+                 ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                 ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
+                 ['mr', 'licks', 'ate', 'my', 'steak', 'how', 'to', 'stop', 'him'],
+                 ['quit', 'buying', 'worthless', 'dog', 'food', 'stupid']]
+    classVec = [0,1,0,1,0,1]    #1 is abusive, 0 not
+    return postingList,classVec
+
+def createVocabList(dataSet):
+    # 将所有文档所有（去重的）词都存到一个列表中，可用set()函数去重。
+    # 用上set()函数操作符号|，取并集，或者写两重循环用vocabSet.add()
+    vocabSet = set([])
+    for document in dataSet:
+        vocabSet = vocabSet | set(document)
+    return  list(vocabSet)
