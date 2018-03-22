b86d4b98e14c0d479e203f681a669a8e484859fd
zxh <zxh_sea@outlook.com>
2018-03-22 16:49:26 +0800

    1

--- 04/bayes.py
+++ 04/bayes.py
@@ -52,13 +52,42 @@ myVocabList = createVocabList(listOPosts)
 def trainNB0(trainMatrix,trainCategory): #输入参数为文档矩阵trainMatrix,文档类别所构成的向量trainCategory
     #计算文档的数目
     numTrainDocs = len(trainMatrix)
-    print(numTrainDocs)
-    # 计算单词的数目
+    # 计算每个文档单词的数目 每个文档单词的数目是一样的
     numWords = len(trainMatrix[0])
+    #有侮辱人单词的文档所占的概率
+    pAbusive = sum(trainCategory)/float(numTrainDocs)
+    p0Num = ones(numWords)#没有侮辱性文档词向量
+    p1Num = ones(numWords) #侮辱性文档词向量
+    p0Denom = 2.0#所有无侮辱性文档 词总数
+    p1Denom = 2.0#所有侮辱性文档 词总数
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Denom += trainMatrix[i]
+            p1Num += sum(trainMatrix[i])
+        else:
+            p0Denom += trainMatrix[i]
+            p0Num += sum(trainMatrix[i])
+    p1Vect = log(p1Num/p1Denom) #修改取对数避免下溢或舍入导致误差
+    p0Vect = log(p0Num/p0Denom)
+    return p0Vect,p1Vect,pAbusive
 
 trainMat = []
 for postinDoc in listOPosts:
     trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
-print(trainMat)
-trainNB0(array(trainMat),array(listClasses))
 
+p0v,p1v,pAb =trainNB0(array(trainMat),array(listClasses))
+print(p0v)
+
+def classifyNB(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1 = sum(vec2Classify*p1Vec) + log(pClass1)
+    p0 = sum(vec2Classify*p0Vec) + log(pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts,listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postinDoc in listOPosts:
